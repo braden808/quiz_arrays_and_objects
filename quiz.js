508ebba478e22382f06e12ee4bd459a42a935f42
Braden Amorozo <braden808@gmail.com>
2016-12-07 08:34:52 -1000

    update

---
+++ quiz.js
@@ -0,0 +1,19 @@
+var myArray = [10, true, "sup", "uce"];
+ console.log(myArray [0]);
+
+function boolean(x) {
+	if myArray == myArray[1]{
+		console.log(myArray[2] + " " + myArray[3]);
+	} else {
+		console.log("you shall not pass!!!");
+	}
+}
+
+boolean(myArray)
+
+var batman = {
+	Man: true,
+	utilityBelt: ["batarang", "smoke bomb"],
+	vehicle: "batmobile",
+	sidekick: "robin"
+}
